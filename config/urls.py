c0b09cc177a19a332c61c57647ff155a86fcbae5
eyob tariku <eyobtariku@gmail.com>
2019-11-29 00:24:54 +0300

    Add a default landing page in shared app

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='shared/default.html')),
     path('admin/', admin.site.urls)
 ]
 
