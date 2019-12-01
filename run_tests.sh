8ccf0cfd6020c351c12ae2d43239365e93e9686b
eyob tariku <eyobtariku@gmail.com>
2019-12-02 01:35:50 +0300

    Add a docker-compose-travis.yml file

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-while ! docker-compose run web nc -z $DB_HOST $DB_PORT; do
-    sleep 2s
+# Check if database is ready
+echo 'Waiting for database....'
+while ! nc -z $DB_HOST $DB_PORT; do
+    sleep 1s
 done
 
-docker-compose run web python manage.py test --no-input
+echo 'Database is ready...'
+
+# Run migrations
+echo 'Run migrations...'
+python manage.py makemigrations  # TODO: Remove later
+
+# Run all tests
+echo 'Run tests...'
+python manage.py test --no-input
+
+exit
