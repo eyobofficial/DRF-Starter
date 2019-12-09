41b3b49852cfec0654972697b5c4bbe3550de876
eyob tariku <eyobtariku@gmail.com>
2019-12-09 22:45:30 +0300

    Modify the deploy.sh file to test SSH

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-openssl aes-256-cbc -K $encrypted_a83ec8a93e11_key -iv $encrypted_a83ec8a93e11_iv \
-    -in deploy_key.enc -out ./deploy_key -d
-chmod 600 ./deploy_key
-ssh-add ./deploy_key
-eval "$(ssh-agent -s)"
-echo -e "Host $SERVER_IP_ADDRESS\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-ssh -i ./deploy_key root@$SERVER_IP_ADDRESS pwd
-
-echo 'Logged...'
-echo $HOSTNAME
-touch check.txt
+touch file.txt
