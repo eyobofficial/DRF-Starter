bd531326a66a1b14f36558dcfa30b4c8ea34f211
eyob tariku <eyobtariku@gmail.com>
2019-12-09 21:10:35 +0300

    Add deploy phase in .travis.yaml

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+openssl aes-256-cbc -K $encrypted_a83ec8a93e11_key -iv $encrypted_a83ec8a93e11_iv \
+    -in deploy_key.enc -out ./deploy_key -d
+chmod 600 ./deploy_key
+ssh-add ./deploy_key
+eval "$(ssh-agent -s)"
+echo -e "Host $SERVER_IP_ADDRESS\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+ssh -i ./deploy_key root@$SERVER_IP_ADDRESS pwd
+touch check.txt
